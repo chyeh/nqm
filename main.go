55d26989c08c26e258db76d67f03543aa6345390
chyeh <chyeh@cepave.com>
2016-05-17 02:23:29 +0000

    [OWL-614] Make it periodic
    It puts the probes into goroutines in an infinite for loop which is
    excuted every one minute.

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "log"
+import (
+	"log"
+	"time"
+)
 
 func init() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
@@ -10,14 +13,20 @@ func main() {
 	InitGeneralConfig()
 	InitRPC()
 
-	probingCmd, targets, agentPtr, err := QueryTask()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	log.Println("Execution the probing command:", probingCmd[0:9])
+	for {
+		go func() {
+			probingCmd, targets, agentPtr, err := QueryTask()
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			log.Println("Execution the probing command:", probingCmd[0:9])
 
-	rawData := Probe(probingCmd)
-	jsonParams := MarshalIntoParameters(rawData, targets, agentPtr)
-	Push(jsonParams)
+			rawData := Probe(probingCmd)
+			jsonParams := MarshalIntoParameters(rawData, targets, agentPtr)
+			Push(jsonParams)
+		}()
+		time.Sleep(time.Minute)
+	}
+	select {}
 }
